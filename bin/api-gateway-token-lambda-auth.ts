648077df5a1bad488681458b12ae457644769d1d
Gregg Ellis Schofield <schofieldgregg@gmail.com>
2022-05-27 12:08:58 +0100

    Read AWS account and region information from CLI config

--- bin/api-gateway-token-lambda-auth.ts
+++ bin/api-gateway-token-lambda-auth.ts
@@ -5,17 +5,14 @@ import { ApiGatewayTokenLambdaAuthStack } from '../lib/api-gateway-token-lambda-
 
 const app = new cdk.App();
 new ApiGatewayTokenLambdaAuthStack(app, 'ApiGatewayTokenLambdaAuthStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+    /*
+     * Deploy infrastructure defined within this app's stacks in the AWS account/region combination
+     * implied by the current CLI configuration.
+     */
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION
+    }
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
